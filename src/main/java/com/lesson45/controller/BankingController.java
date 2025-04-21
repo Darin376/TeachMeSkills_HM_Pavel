cae39ab089a57511077a1c819333fe0e60bc3733
Pavel Shcuratov <Sleeper2012@yandex.ru>
2025-04-21 18:33:50 +0300

    Aspect java Spring/boot sql/2

--- src/main/java/com/lesson45/controller/BankingController.java
+++ src/main/java/com/lesson45/controller/BankingController.java
@@ -65,3 +65,96 @@ public class BankingController {
 
 
 }
+
+
+//CREATE TABLE bankUser (
+//        id SERIAL PRIMARY KEY,
+//        name VARCHAR(50) UNIQUE NOT NULL
+//);
+//
+//
+//CREATE TABLE userCard (
+//        id SERIAL PRIMARY KEY,  -- Уникальный ID для каждой машины
+//        user_id INT NOT NULL,   -- Ссылка на владельца (без UNIQUE)
+//card  VARCHAR(50) NOT NULL,
+//FOREIGN KEY (user_id) REFERENCES bankUser(id)
+//        );
+//
+//ALTER TABLE userCard
+//ADD COLUMN money DECIMAL(15, 2) NOT NULL DEFAULT 0.00;
+//
+//INSERT INTO bankUser (name) VALUES
+//('ирина Пет'),
+//        ('Анна Сидорова')
+//
+//INSERT INTO userCard (user_id, card, money) VALUES
+//(3, '1111  22222 3333 4444', 5000.50),   -- Карта пользователя 1
+//        (1, '5536 9132 4567 1234', 5000.00),    -- Вторая карта пользователя 1
+//        (2, '5168 7423 9876 5432', 23000.75)   -- Карта пользователя 2
+//
+//
+//SELECT * FROM bankUser;
+//SELECT * FROM userCard;
+//
+//
+//SELECT b.id, b.name, u.money
+//FROM bankUser b
+//JOIN userCard u ON b.id = u.user_id
+//WHERE b.id = 1;
+//
+//
+//BEGIN;
+//
+//-- Блокируем строки для изменения
+//SELECT money FROM userCard WHERE id = 1 FOR UPDATE;
+//SELECT money FROM userCard WHERE id = 2 FOR UPDATE;
+//
+//-- Проверка баланса
+//DO $$
+//DECLARE
+//from_balance DECIMAL(15, 2);
+//BEGIN
+//SELECT money INTO from_balance FROM userCard WHERE id = 1;
+//IF from_balance < 1000.00 THEN
+//RAISE EXCEPTION 'Недостаточно средств';
+//END IF;
+//END $$;
+//
+//-- Перевод
+//UPDATE userCard SET money = money - 1000.00 WHERE id = 1;
+//UPDATE userCard SET money = money + 1000.00 WHERE id = 2;
+//
+//COMMIT;
+//
+//SELECT money FROM userCard WHERE user_id = 2 AND card = '5536 9132 4567 1234';
+//UPDATE userCard SET money = 9000 WHERE user_id = 2 AND card = '5536 9132 4567 1234';
+//
+//UPDATE userCard SET money = money + 500 WHERE card = '5536 9132 4567 1234';
+//
+//
+//SELECT * FROM userCard WHERE card = '4276 3800 1234 2221';
+//
+//SELECT * FROM userCard
+//WHERE card IN ('4276 3800 1234 2221', '5536 9132 4567 1234');
+//SELECT * FROM userCard WHERE card = '5536 9132 4567 1234';
+//SELECT * FROM bankUser WHERE id = 1;
+//SELECT u.id as user_id, u.name, c.card, c.money
+//FROM bankUser u
+//JOIN userCard c ON u.id = c.user_id;
+//
+//CREATE INDEX idx_user_card ON userCard(card);
+//CREATE INDEX idx_user_id_card ON userCard(user_id, card);
+//
+//
+//
+//SELECT money FROM userCard WHERE user_id = 2 AND card = '5536 9132 4567 1234'
+//SELECT money FROM userCard WHERE user_id = 1 AND card = '4276 3800 1234 2221'
+//SELECT id FROM userCard WHERE card = '5536 9132 4567 1234'
+//UPDATE userCard SET money = money - 1000 WHERE user_id = 1 AND card = '4276 3800 1234 2221'
+//UPDATE userCard SET money = money + 1000 WHERE card = '5536 9132 4567 1234'
+//
+//SELECT * FROM userCard;
+
+
+//
+//sql код для таблиц
