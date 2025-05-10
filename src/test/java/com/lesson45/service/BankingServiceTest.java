93a554f18f2af8c8dfc848da0293b671c90ca944
Pavel Shcuratov <Sleeper2012@yandex.ru>
2025-05-10 18:22:08 +0300

    Aspect java Spring/boot sql/6

---
+++ src/test/java/com/lesson45/service/BankingServiceTest.java
@@ -0,0 +1,14 @@
+package com.lesson45.service;
+
+import static org.junit.Assert.*;
+
+public class BankingServiceTest {
+
+    @org.junit.Test
+    public void getUserById() {
+    }
+
+    @org.junit.Test
+    public void transfer() {
+    }
+}
